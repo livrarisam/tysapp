ea6611918ffa86726cc331732df5ce7693ce165a
Samuel Xavier <samuel@elav.net.br>
2014-11-12 13:16:06 -0200

    .

--- www/js/index.js
+++ www/js/index.js
@@ -43,25 +43,23 @@ var app = {
             );            
         });
 
-        $("#frmCadastro").submit(function(e){
+        $("#btn_login").on("click", function(e) {
+            e.preventDefault();
             var nome = $("#nome", this).val();
             var login = $("#login", this).val();
             var email = $("#email", this).val();
             var senha = $("#senha", this).val();
             $.post("http://walkey.com.br/api/usuarios/create", {data: '{"nome":"'+nome+'","sobrenome":"'+login+'","email":"'+email+'","senha":"'+senha+'"}'}, 
                 function(data) {
-                    if (data.result == "erro") {
-                        e.preventDefault();
-                        navigator.notification.alert(data.error_string, function() {});
-                        return false;
-                    } else {
+                    if (data.result = "sucesso") {
                         window.localStorage["idUsuario"] = data.idUsuario;
                         window.localStorage["nome"] = data.nome;
                         window.localStorage["sobrenome"] = data.sobrenome;
                         window.localStorage["email"] = data.email;
                         window.localStorage["logged"] = true;
-                        navigator.notification.alert("Cadastro OK", function() {});
-                        return true;
+                        navigator.notification.alert("Cadastro efetuado com sucesso", function() { $("#frm_cadastro").submit() });
+                    } else {
+                        navigator.notification.alert(data.error_string, function() {});
                     }
                 }, "json"
             );
