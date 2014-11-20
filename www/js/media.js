541a20fbbc6f8fb6cbf2a0d1824949de6b4cfba8
Samuel Xavier <samuelxavi@gmail.com>
2014-11-20 17:36:42 -0200

    .

--- www/js/media.js
+++ www/js/media.js
@@ -5,12 +5,11 @@ var player = {
 
     initialize: function() {
         player.audio_escuro = new Media('/android_asset/www/musicas/escuro.mp3');
-        player.audio_escuro_rapido = new Media('/android_asset/www/musicas/escuro_rapido.mp3');
-        setTimeout(player.playEscuro(), 5000);
+        setTimeout(function() { player.playEscuro(); }, 5000);
     },
 
     playEscuro: function() {
-        audio_escuro.play();
+        player.audio_escuro.play();
         // audio_escuro_rapido.stop();
     }
 
