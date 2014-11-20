eb0b6d1a3e1354e3ef34f5cd36d0c202e08ed475
Samuel Xavier <samuelxavi@gmail.com>
2014-11-20 18:33:31 -0200

    .

--- www/js/media.js
+++ www/js/media.js
@@ -6,23 +6,33 @@ var player = {
     initialize: function() {
         player.audio_escuro = new Media('/android_asset/www/musicas/escuro.mp3');
         player.audio_sol    = new Media('/android_asset/www/musicas/sol.mp3');
-        player.audio_escuro.setVolume('0.0');
-        player.audio_sol.setVolume('0.0');
         player.audio_escuro.play();
+        player.audio_escuro.setVolume('0.0');
         player.audio_sol.play();
-        setTimeout(function() { player.playEscuro(); }, 5000);
+        player.audio_sol.setVolume('0.0');
+        setTimeout(function() { player.playEscuro(); }, 3000);
     },
 
     playEscuro: function() {
         player.audio_sol.setVolume('0.0');
         player.audio_escuro.setVolume('1.0');
-        setTimeout(function() { player.playSol(); }, 3000);
+        setTimeout(function() { player.lowEscuro(); }, 5000);
     },
+
+    lowEscuro: function() {
+        player.audio_escuro.setVolume('0.2');
+        setTimeout(function() { player.playSol(); }, 5000);
+    }
 
     playSol: function() {
         player.audio_sol.setVolume('1.0');
         player.audio_escuro.setVolume('0.0');        
-        setTimeout(function() { player.playEscuro(); }, 3000);
+        setTimeout(function() { player.lowSol(); }, 5000);
+    },
+
+    lowSol: function() {
+        player.audio_sol.setVolume('0.2');
+        setTimeout(function() { player.playSol(); }, 5000);
     }
 
 }
