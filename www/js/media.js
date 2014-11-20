a4e2b5da29ab56640e194977047b9633348d77dd
Samuel Xavier <samuelxavi@gmail.com>
2014-11-20 18:42:36 -0200

    .

--- www/js/media.js
+++ www/js/media.js
@@ -4,8 +4,8 @@ var player = {
     audio_sol: null,
 
     initialize: function() {
-        player.audio_escuro = new Media('/android_asset/www/musicas/escuro.mp3');
-        player.audio_sol    = new Media('/android_asset/www/musicas/sol.mp3');
+        player.audio_escuro = new Media('/android_asset/www/musicas/escuro.mp3', player.nothing, player.nothing, player.onStatus);
+        player.audio_sol    = new Media('/android_asset/www/musicas/sol.mp3', player.nothing, player.nothing, player.onStatus);
         player.audio_escuro.play();
         player.audio_escuro.setVolume('0.0');
         player.audio_sol.play();
@@ -22,17 +22,25 @@ var player = {
     lowEscuro: function() {
         player.audio_escuro.setVolume('0.2');
         setTimeout(function() { player.playSol(); }, 5000);
-    }
+    },
 
     playSol: function() {
-        player.audio_sol.setVolume('1.0');
         player.audio_escuro.setVolume('0.0');        
+        player.audio_sol.setVolume('1.0');
         setTimeout(function() { player.lowSol(); }, 5000);
     },
 
     lowSol: function() {
         player.audio_sol.setVolume('0.2');
-        setTimeout(function() { player.playSol(); }, 5000);
+        setTimeout(function() { player.playEscuro(); }, 5000);
+    },
+
+    nothing: function() {
+        //zzz
+    },
+
+    onStatus: function() {
+        //zzz
     }
 
 }
